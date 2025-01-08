b463264302abdc06bddd47e23ebc2c24f6b0c8b6
Ruan Phablo <ruanphablo778@gmail.com>
2025-01-08 19:43:02 -0300

    Feat: Create GameLoop

--- src/main/java/com/github/yoruhinda/aurakingdom/core/game/gameloop/Game.java
+++ src/main/java/com/github/yoruhinda/aurakingdom/core/game/gameloop/Game.java
@@ -2,8 +2,25 @@ package main.java.com.github.yoruhinda.aurakingdom.core.game.gameloop;
 
 import main.java.com.github.yoruhinda.aurakingdom.core.game.window.GameFrame;
 
-public class Game {
+public class Game extends Thread{
+    private boolean running;
     public Game(){
         new GameFrame();
+    }
+
+    @Override
+    public void run() {
+        while(running){
+            update();
+            render();
+        }
+    }
+
+    private void update(){
+
+    }
+
+    private void render(){
+
     }
 }
