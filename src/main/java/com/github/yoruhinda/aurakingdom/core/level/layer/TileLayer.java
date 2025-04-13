9d8bc22cf5dee1834351c06bfb54b4c89ebaa92c
Ruan Phablo <ruanphablo778@gmail.com>
2025-04-12 22:11:56 -0300

    Chore: Refatoring!

--- src/main/java/com/github/yoruhinda/aurakingdom/core/level/layer/TileLayer.java
+++ src/main/java/com/github/yoruhinda/aurakingdom/core/level/layer/TileLayer.java
@@ -1,4 +1,16 @@
 package main.java.com.github.yoruhinda.aurakingdom.core.level.layer;
 
+import java.awt.image.BufferedImage;
+import java.util.List;
+
 public class TileLayer {
+    private int id;
+    private String name;
+    private int[][] data;
+    private List<BufferedImage> tiles;
+
+    public TileLayer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
