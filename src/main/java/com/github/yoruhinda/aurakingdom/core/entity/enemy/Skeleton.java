20fa71210e60facf64bc042826db153c3713aee7
Ruan Phablo <ruanphablo778@gmail.com>
2026-08-15 23:46:56 -0300

    Chore: enemy

--- src/main/java/com/github/yoruhinda/aurakingdom/core/entity/enemy/Skeleton.java
+++ src/main/java/com/github/yoruhinda/aurakingdom/core/entity/enemy/Skeleton.java
@@ -1,4 +1,9 @@
 package com.github.yoruhinda.aurakingdom.core.entity.enemy;
 
-public class Skeleton {
+import com.github.yoruhinda.aurakingdom.core.entity.Entity;
+
+public class Skeleton extends Entity {
+    protected Skeleton(int x, int y) {
+        super(x, y);
+    }
 }
