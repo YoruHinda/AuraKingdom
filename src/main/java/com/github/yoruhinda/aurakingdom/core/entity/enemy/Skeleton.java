ee95bea52a775801e622f878d6212dd5b03c1507
Ruan Phablo <ruanphablo778@gmail.com>
2026-08-18 21:51:31 -0300

    Chore: Skeleton!

--- src/main/java/com/github/yoruhinda/aurakingdom/core/entity/enemy/Skeleton.java
+++ src/main/java/com/github/yoruhinda/aurakingdom/core/entity/enemy/Skeleton.java
@@ -1,8 +1,10 @@
 package com.github.yoruhinda.aurakingdom.core.entity.enemy;
 
+import com.github.yoruhinda.aurakingdom.core.animation.Animation;
 import com.github.yoruhinda.aurakingdom.core.entity.Entity;
 
 public class Skeleton extends Entity {
+    private Animation animation;
     protected Skeleton(int x, int y) {
         super(x, y);
     }
