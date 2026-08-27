67347b7f7409af4e64937092247a421364e494c8
Ruan Phablo <ruanphablo778@gmail.com>
2026-08-26 22:10:24 -0300

    Chore: Skeleton!

--- src/main/java/com/github/yoruhinda/aurakingdom/core/util/resourcedirectory/SkeletonDirectory.java
+++ src/main/java/com/github/yoruhinda/aurakingdom/core/util/resourcedirectory/SkeletonDirectory.java
@@ -1,4 +1,10 @@
 package com.github.yoruhinda.aurakingdom.core.util.resourcedirectory;
 
 public class SkeletonDirectory {
+    public static final String ATTACK = "Skeleton/Attack";
+    public static final String DEATH = "Skeleton/Death";
+    public static final String HIT = "Skeleton/Hit";
+    public static final String IDLE = "Skeleton/Idle";
+    public static final String WALK = "Skeleton/Walk";
+    public static final String REACT = "Skeleton/React";
 }
